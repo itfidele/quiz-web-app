d1fe0da413ee889be9b4aaa98a9a0ec09907968f
Fidele K <itfidele@gmail.com>
2023-11-16 01:53:48 +0300

    Remove GitHub Pages deployment workflow and update Next.js config.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/quiz-web-app",
-    images: {
-        unoptimized: true,
-    },
 }
 
 module.exports = nextConfig
