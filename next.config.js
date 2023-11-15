803455a18bf33d11ce75b56e455e580c52283cc3
Fidele K <itfidele@gmail.com>
2023-11-16 01:25:10 +0300

    Add output, basePath, and images options to next.config.js and remove engines from package.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    basePath: "/quiz-web-app",
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
