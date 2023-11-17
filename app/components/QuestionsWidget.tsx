f426a79d85cdf7921414fe4841852612674c8fc0
Fidele K <itfidele@gmail.com>
2023-11-17 14:34:17 +0300

    Refactor QuestionWidget component and add onResult prop

--- app/components/QuestionsWidget.tsx
+++ app/components/QuestionsWidget.tsx
@@ -5,28 +5,39 @@ import { Box, Button, Flex, Spinner, Text } from "@chakra-ui/react";
 import React, { Suspense } from "react";
 import LoadingSpinner from "./LoadingSpinner";
 
+// how to pass a function with prop type
+export interface QuizResult{
+    question:Question,
+    isCorrect:boolean,
+    selectedAnswer:string
+}
 
-export default function QuestionWidget({ questions }:{ questions:Question[]}){
+
+export default function QuestionWidget({ questions,onResult }:{ questions:Question[],onResult:(results:QuizResult[])=>void}){
     
     const [currentQuestion,setCurrentQuestion] = React.useState(0); 
     const [selectedAnswer,setSelectedAnswer] = React.useState<number>(-1);
-    const [quiz,setQuiz] = React.useState<Quiz>(new Quiz(questions));
-    const [score,setScore] = React.useState(0);
-    const [currentAnswers,setCurrentAnswers] = React.useState<string[]>([]);
-    const [isRandomizing,setIsRandomizing] = React.useState(true);
+    const [results,setResults] = React.useState<QuizResult[]>([]);
+    const [question,setQuestion] = React.useState<Question>(questions[currentQuestion]);
+    const quiz = new Quiz(questions);
+    const isLastQuestion = currentQuestion == questions.length-1;
     
-
-    React.useEffect(()=>{
-        
-        setCurrentAnswers(quiz.getChoices(currentQuestion));
-        setIsRandomizing(false);
-    },[quiz,currentQuestion]);
-    
-    
-
-    function next(){
+    const onSubmit = ()=>{
+        setResults([...results,{question:question,isCorrect: question.answers[currentQuestion] == question.correctAnswer,selectedAnswer:question.answers[currentQuestion]}]);
         setSelectedAnswer(-1);
-        setCurrentQuestion(currentQuestion+1);
+        alert(results.length)
+        onResult(results);
+    }
+    
+
+    function next(index:number){
+        setResults([...results,{question:question,isCorrect: question.answers[index] == question.correctAnswer,selectedAnswer:question.answers[index]}]);
+        setSelectedAnswer(-1);
+        if (!isLastQuestion) {  
+            setQuestion(questions[currentQuestion+1]);
+            setCurrentQuestion(currentQuestion+1);
+        }
+        
     }
 
     function prev(){
@@ -34,28 +45,25 @@ export default function QuestionWidget({ questions }:{ questions:Question[]}){
     }
 
     return <>
-        { isRandomizing ? <LoadingSpinner/>: <Flex w={"100%"} gap={6} p={6} rounded={"md"} bg={"#4950AE"} direction={"column"}>
+        <Flex w={"100%"} gap={4} p={6} rounded={"md"} bg={"#4950AE"} direction={"column"}>
 
             <Flex flex={1} direction={"column"} w={"full"}>
-                <h1>Question {currentQuestion+1} of {quiz.length}  {quiz.getQuestion(currentQuestion).correctAnswer}  {quiz.isCorrect(selectedAnswer,currentAnswers[selectedAnswer]) }  {selectedAnswer} </h1>
-                <Text fontWeight={"bold"}>{quiz.getQuestion(currentQuestion).question}</Text>
+                <h1>Question {currentQuestion+1} of {questions.length}</h1>
+                <Text fontWeight={"bold"}>{question.question}</Text>
                 <Flex my={4} direction={"column"} gap={4} width={"full"} justifyContent={"start"}>
                     {  
-                        currentAnswers.map((answer,index)=>{
-                            return <Flex gap={4} border={2} alignItems={"center"} onClick={()=>setSelectedAnswer(index)} bg={ quiz.isCorrect(selectedAnswer,answer) ?"#44C651": selectedAnswer == index ? "#E24040":""} fontSize={"sm"} borderColor={"#8F95EE"} cursor={"pointer"} _hover={{bg:"#8F95EE"}} px={4} py={2} key={index}>
-                                <Text fontSize={30}>{index+1} </Text>
-                                <Flex flex={1}>{answer.replace('"',"")}</Flex>
-                                <Flex>{index}</Flex>
+                        question.answers.map((answer,index)=>{
+                            return <Flex style={{userSelect: 'none'}} gap={4} borderStyle={"solid"} rounded={"xl"} transition={"all"} transitionDuration={"300"} border={"1px"}  alignItems={"center"} onClick={ currentQuestion>=0 && !isLastQuestion ?()=>next(index): ()=>setSelectedAnswer(index)} bg={ selectedAnswer == index ? "#8F95EE":""} fontSize={"sm"} borderColor={"white"} cursor={"pointer"} _hover={{bg:"#8F95EE"}} px={4} py={2} key={index}>
+                                {answer}
                             </Flex>
                         }) }
                 </Flex>
             </Flex>
+            
+            { isLastQuestion && <Flex justifyContent={"center"}>
+                <Button  variant={"solid"} w={"70%"} bg={"#3B199C"} rounded={"full"} onClick={onSubmit} disabled={selectedAnswer == -1}>Check Your Results</Button>
+            </Flex>}
 
-            <Flex gap={4} w={"full"}>
-                { currentQuestion != 0 && <Button rounded={"full"} w={"full"} onClick={prev}>back</Button> }
-                { currentQuestion != quiz.length-1 && <Button rounded={"full"} w={"full"} bg={"#3B199C"} onClick={next}>Skip Question</Button> } 
-            </Flex>
-
-        </Flex>}
+        </Flex>
     </>
 }
